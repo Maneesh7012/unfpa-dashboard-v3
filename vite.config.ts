f054fd124d2809610b5941936b021fba5a68f900
hridayanp <hridayanphukan123@gmail.com>
2026-04-29 21:20:46 +0530

    extra

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/v5',
+  base: '/v5/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
